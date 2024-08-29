5166b177713833b1860f3d72265ce6470f6064ac
guntherhaas <guntherhaas.eng@gmail.com>
2024-08-29 14:48:37 +0200

    initial commit

---
+++ src/test/java/haas/gunther/universal_pet_care/UniversalPetCareApplicationTests.java
@@ -0,0 +1,13 @@
+package haas.gunther.universal_pet_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalPetCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
